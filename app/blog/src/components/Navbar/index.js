84c265ad3a8aaf18403e04a137e557b3dffba94e
Yass <bargach.y@gmail.com>
2024-08-15 10:19:21 +0200

    update navbar

--- app/blog/src/components/Navbar/index.js
+++ app/blog/src/components/Navbar/index.js
@@ -31,20 +31,20 @@ const Navbar = () => {
 
 	return (
 	<nav className="flex fixed backdrop-blur w-full z-10 top-0 left-0 border-b border-gray-800">
-		<div className="flex ml-4 mx-auto min-h-[5vh] p-0 bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-secondary text-md pr-10">
+		<div className="flex ml-4 mx-auto min-h-[5vh] p-0 bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-secondary text-md pr-10 mt-3">
 						<Link to={"/"}>
 						Ayzminy.blog
 						</Link>
 		</div>
 
 		{/* Desktop navigation */}
-		<div className= "hidden md:flex sm:flex-row sm:gap-4 gap-4 sm:w-auto items-center justify-center mx-auto w-full sm:min-h-[5vh] min-h-[5vh] sm:block md:w-auto">
-				<ul className='flex m-2 p-0 mt-3 rounded-lg sm:flex-row space-x-8 sm:mt-0 border-0 dark:border-gray-700'>
+		<div className= "hidden md:flex sm:flex-row sm:gap-4 gap-4 sm:w-auto right-0 w-full sm:min-h-[5vh] min-h-[5vh] sm:block md:w-auto">
+				<ul className='flex m-2 p-0 mt-5 rounded-lg sm:flex-row space-x-8 sm:mt-0 border-0 dark:border-gray-700 pr-10'>
 					{navLinks.map((nav, index) => (
 					<li key={nav.name}
-						className="p-3">
+						className="pt-3">
 							<Link className="sm:block hover:rounded-md sm:p-1 text-secondary hover:shadow-secondaryone border-secondary 
-							  hover:text-secondary hover:bg-gray-800 duration-300 opacity-70 p-3 hover:opacity-90" to={`${nav.link}`}>{nav.name}</Link>
+							  hover:text-secondary hover:bg-gray-800 duration-300 opacity-70 hover:opacity-90" to={`${nav.link}`}>{nav.name}</Link>
 						</li>
 					))}
 
@@ -52,7 +52,7 @@ const Navbar = () => {
 		</div>
 
 		{/* Mobile navigation */}
-		<div onClick={handleNav} className='fixed sm:hidden top-0 right-0 pr-3 pt-5'>
+		<div onClick={handleNav} className='fixed sm:hidden top-0 right-0 pr-3 pt-3'>
 			{nav ? <AiOutlineClose className='text-secondary' size={20} /> : <AiOutlineMenu className='text-secondary' size={20} />}
 		</div>
 
@@ -60,7 +60,7 @@ const Navbar = () => {
 		<ul
 			className={
 			nav
-				? 'fixed right-0 top-0 h-full border-secondary ease-in-out duration-500 pr-3 pl-3 mt-10 w-full text-center'
+				? 'fixed right-0 top-0 h-full border-secondary ease-in-out duration-500 pr-3 pl-3 mt-10 w-full'
 				: 'ease-in-out duration-500 fixed right-0 hidden'
 			}
       	>
@@ -71,8 +71,8 @@ const Navbar = () => {
 				key={nav.name}
 				className='p-4 duration-300 border-[1px] m-[1px] border-gray-800 hover:text-black bg-primary'
 			>
-				<Link className="sm:block hover:rounded sm:p-1 text-secondary hover:shadow-secondaryone border-secondary 
-					hover:text-secondaryduration-300 opacity-70 p-3 hover:opacity-90" to={`${nav.link}`}>{nav.name}</Link>
+				<Link onClick={handleNav} className="sm:block hover:rounded sm:p-1 text-secondary hover:shadow-secondaryone border-secondary 
+					hover:text-secondary duration-300 opacity-70 p-3 hover:opacity-90" to={`${nav.link}`}>{nav.name}</Link>
 			</li>
 			))}
       </ul>
--- app/blog/src/pages/Contact.js
+++ app/blog/src/pages/Contact.js
@@ -36,22 +36,21 @@ export default function Contact() {
             </div>
 
             <div className="flex">
-                <h1 className="text-white pl-5 sm:pl-40 pt-20 font-extrabold text-2xl underline decoration-indigo-500 hover:decoration-indigo-800">Whoami [Solely written with chatGPT]</h1>
+                <h1 className="text-white pl-5 sm:pl-40 pt-20 font-extrabold text-2xl underline decoration-indigo-500 hover:decoration-indigo-800">Whoami?</h1>
             </div>
             <div className="text-gray-500 pl-5 pr-5 sm:pl-40 sm:pr-40">
-            Hi, my name's Yass, your friendly neighborhood cybersecurity player, and a whole lot more! 🤓<br />
+            Hey wait there curious fella 🤓! Stop trying to get information about me !<br />
 
-            Born with a keyboard in one hand and a curious mind in the other, I've been riding the digital waves since I can remember. You can call me a true-blue internet explorer, both in terms of code and culture.<br />
-            <span>🔒 Cybersecurity Connoisseur 🖥️</span> <br />
+            I'm just an engineer that works in infosec. I'm not shitty but I'm not the best either ! <br />
+            People often ask me some random questions like...<br />
+            <span>🔒 Are you a Cybersecurity Connoisseur 🖥️?</span> <br />
+            Yes, indeed. I am.<br />
+            <span>🧠 Can you hack phones and stuff?</span><br />
 
-            First things first, I'm the guy you want on your side when it comes to locking down those ones and zeros. I've spent more time than I'd like to admit in the battle against cyber threats. From defending networks to poking holes in them (with permission, of course), I'm all about making the digital world a safer place. Hackers, take your best shot! 💻🛡️ <br />
+            I can do better. I can steal it from you if you get close enough.<br />
             
-            <span>🧠 Forever a Knowledge Junkie 📚</span><br />
-
-            But hey, my love for tech doesn't stop at firewalls and encryption keys. I'm a certified knowledge junkie, diving into all kinds of subjects. From AI to astrophysics, there's always something new to learn. After all, curiosity never killed this cat!   <br />
-            
-            🏄‍♂️ Surfing: Where Gravity and I Have a Love-Hate Relationship 🌊<br />
-            When I'm not at the keyboard, you'll find me attempting to master the art of surfing. I'm not exactly carving epic turns like a pro – more like carving a path to the sand. But hey, the ocean's my happy place, even when I'm belly flopping into it. Catching waves and catching some serene moments, that's the name of my game. 🤙<br />
+            🏄‍♂️ How are your skills in dealing with waves ? 🌊<br />
+            Stop asking hurtful questions. 🤙<br />
             </div>
         </div>
     )
