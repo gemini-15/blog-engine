0047ca130a23d552b92b0f1de6e9f8b4504ecfd1
Yass <bargach.y@gmail.com>
2024-10-16 23:37:46 +0200

    fixing docker compose

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -10,3 +10,12 @@ diesel migration run
 # diesel migration generate create_articles
 # diesel migration redo
 /bin/server 
+
+# $DB_PASSWD=$(cat /run/secrets/db-password)
+
+# export DATABASE_URL="postgresql://$POSTGRES_USER:$DB_PASSWD@postgres:5432/$POSTGRES_DB" cargo run 
+
+
+# diesel migration run
+
+# /bin/server
--- scripts_sql/01_users.sql
+++
@@ -1,1 +0,0 @@
-ALTER ROLE core SUPERUSER;
